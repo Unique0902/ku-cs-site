64d8f1b1f9a82c9b052db98413d19d5e1493892b
mook <rla0591@gmail.com>
2023-01-12 16:46:23 +0900

    add recruitmentInformationData, departmentNewsData

---
+++ data/recruitmentInformation.js
@@ -0,0 +1,52 @@
+const recruitmentInformationData = [
+  {
+    title: '[경원테크] 인턴 사원 및 정규직 채용 안내',
+    content: '',
+    author: '관리자',
+    time: '2023-01-12',
+    view: 5,
+    isNotice: false,
+  },
+  {
+    title:
+      '[한국지능정보사회진흥원] 2022년 양자 ICT 분야 포닥 펠로우십 연구원 모집',
+    content: '',
+    author: '관리자',
+    time: '2023-01-11',
+    view: 8,
+    isNotice: false,
+  },
+  {
+    title: '[LG CNS] 2022 CODE MONSTER (채용연계형) (~11.11(금) 오후 2시)',
+    content: '',
+    author: '관리자',
+    time: '2023-01-09',
+    view: 40,
+    isNotice: false,
+  },
+  {
+    title: '[한화/모멘텀] 2022년 채용전제형 인턴 모집 (~11/13)',
+    content: '',
+    author: '관리자',
+    time: '2023-01-06',
+    view: 22,
+    isNotice: false,
+  },
+  {
+    title: '[토스] Admin 개발 Assistant',
+    content: '',
+    author: '관리자',
+    time: '2023-01-06',
+    view: 38,
+    isNotice: false,
+  },
+  {
+    title:
+      '[LG CNS] 2023 LG CNS Winter Internship : 학사 맞춤형 인턴십 (~11.7)',
+    content: '',
+    author: '관리자',
+    time: '2023-01-05',
+    view: 29,
+    isNotice: false,
+  },
+];
