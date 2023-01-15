341d17ac5891d2490261c91124e8dd01a6c460c4
mook <rla0591@gmail.com>
2023-01-15 20:15:02 +0900

    change mainSide width

--- components/mainSide/mainSide.jsx
+++ components/mainSide/mainSide.jsx
@@ -4,7 +4,7 @@ import MainSideBtn from '../mainSideBtn/mainSideBtn';
 export default function MainSide({ data, secondLocation, firstName }) {
   return (
     <aside className='pt-8'>
-      <div className='font-sans border-b border-gray-300 text-green-600 font-bold text-3xl flex justify-center  h-36 w-56 bg-cover bg-circleLogo relative'>
+      <div className='font-sans border-b border-gray-300 text-green-600 font-bold text-3xl flex justify-center  h-48 w-72 bg-cover bg-circleLogo relative'>
         <div className='absolute left-0 top-0 h-full w-full bg-white bg-opacity-75'></div>
         <p className='z-10 mt-8 font-sans text-3xl'>{firstName}</p>
       </div>
