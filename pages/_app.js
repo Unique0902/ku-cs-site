ee6b91b8ed0d038a9c6a98dc9554a73a32cbe290
mook <rla0591@gmail.com>
2023-01-16 23:07:43 +0900

    add head data

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
 import AppFooter from '../components/appFooter';
 import AppHeader from '../components/appHeader';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => <>{page}</>);
   return (
     <>
       <AppHeader />
+      <Head>
+        <title>건국대학교 컴퓨터공학부</title>
+        <meta
+          name='description'
+          content='건국대학교 컴퓨터공학부 사이트입니다.'
+        />
+      </Head>
       {getLayout(<Component {...pageProps} />)}
       <AppFooter />
     </>
